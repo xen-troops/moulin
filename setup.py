8cc5d3b3abedef4a0ed5c1ce2bd67b4a60f02b2e
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2021-06-15 23:53:03 +0300

    moulin: add entry points and setup scripts
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from typing import Dict, Any
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+SETUP_ARGS: Dict[str, Any] = dict(
+    name='moulin',  # Required
+    version='0.1',  # Required
+    description='Meta-build system',  # Required
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/xen-troops/moulin',  # Optional
+    author='Volodymyr Babchuk',  # Optional
+    author_email='volodymyr_babchuk@epam.com',  # Optional
+
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='build ninja yocto android repo git',
+    packages=[
+        "moulin",
+        "moulin.fetchers",
+        "moulin.builders",
+    ],  # Required
+    python_requires=">=3.6",
+    entry_points={'console_scripts': ['moulin = moulin.main:console_entry']})
+setup(**SETUP_ARGS)
