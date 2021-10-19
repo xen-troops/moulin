3b3f01fdef4539929d652259d7f938515982d359
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2021-10-19 14:53:37 +0300

    main.py: switch to a new build conf processor
    As was described in a previous commit, we have a new configuration
    processor which provides better user experience. Switch to in now.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- moulin/main.py
+++ moulin/main.py
@@ -5,19 +5,15 @@ Moulin main entry point
 """
 
 import argparse
-import yaml
 import logging
 import sys
 import importlib_metadata
+import yaml
 from packaging.version import Version
 
 from moulin import build_generator
-from moulin import build_conf_processor
+from moulin.build_conf import MoulinConfiguration
 
-try:
-    from yaml import CLoader as YamlLoader
-except ImportError:
-    from yaml import Loader as YamlLoader
 log = logging.getLogger(__name__)
 
 
@@ -35,32 +31,27 @@ def console_entry():
     parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
 
     args, extra_opts = parser.parse_known_args()
-    conf = yaml.load(open(args.conf), Loader=YamlLoader)
 
-    if "desc" not in conf:
-        raise Exception("'desc' field in config file is mandatory!")
     loglevel = logging.INFO
     if args.verbose:
         loglevel = logging.DEBUG
     logging.basicConfig(level=loglevel, format="[%(levelname)s] %(message)s")
 
-    if "min_ver" in conf:
+    conf = MoulinConfiguration(yaml.compose(open(args.conf)))
+
+    if conf.min_ver:
         our_ver = Version(importlib_metadata.version("moulin"))
-        req_ver = Version(conf["min_ver"])
-        if our_ver < req_ver:
-            raise Exception(
-                f"Config file requires version {req_ver}, while you are running mouilin {our_ver}"
-            )
+        if our_ver < conf.min_ver:
+            raise Exception(f"Config file requires version {conf.min_ver}," +
+                            " while you are running mouilin {our_ver}")
 
     prog = f"{sys.argv[0]} {args.conf}"
-    desc = f"Config file description: {conf['desc']}"
-    config_argparser = argparse.ArgumentParser(description=desc,
-                                               prog=prog,
-                                               add_help=False)
-    for parameter in build_conf_processor.get_possible_parameters(conf):
+    desc = f"Config file description: {conf.desc}"
+    config_argparser = argparse.ArgumentParser(description=desc, prog=prog, add_help=False)
+    for parameter in conf.get_parameters().values():
         config_argparser.add_argument(f"--{parameter.name}",
-                                      choices=parameter.choices,
-                                      default=parameter.default,
+                                      choices=[x.name for x in parameter.variants.values()],
+                                      default=parameter.default.name,
                                       help=parameter.desc)
 
     if args.help_config:
@@ -68,5 +59,8 @@ def console_entry():
         return
 
     config_args = config_argparser.parse_args(extra_opts)
-    conf = build_conf_processor.process_config(conf, vars(config_args))
-    build_generator.generate_build(conf, args.conf)
+    conf.complete_init(vars(config_args))
+    log.info("Generating build.ninja")
+
+    document = conf.get_document()
+    build_generator.generate_build(document, args.conf)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 SETUP_ARGS: Dict[str, Any] = dict(
     name='moulin',  # Required
-    version='0.2',  # Required
+    version='0.3',  # Required
     description='Meta-build system',  # Required
     long_description=long_description,
     long_description_content_type="text/markdown",
