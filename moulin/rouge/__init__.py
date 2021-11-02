ea13a11b0aef44c0da5ff76e998566717d5f3de0
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2021-11-02 23:49:02 +0200

    rouge: add image rules generation
    This patch adds Ninja rules generator, so rouge can be invoked via
    Ninja.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- moulin/rouge/__init__.py
+++ moulin/rouge/__init__.py
@@ -3,10 +3,13 @@
 """
 Rouge Image generator
 """
+import sys
 from typing import List, NamedTuple
 
 from yaml.nodes import MappingNode
 from moulin.yaml_helpers import get_scalar_node, get_mapping_node
+from moulin import ninja_syntax
+from .block_entry import construct_entry
 
 
 class RougeImage(NamedTuple):
@@ -32,3 +35,22 @@ def get_available_images(root_node: MappingNode) -> List[RougeImage]:
             desc = desc_node.value
         ret.append(RougeImage(name, desc, image_node))
     return ret
+
+
+def gen_build_rules(generator: ninja_syntax.Writer):
+    "Generate ninja rules to build images via rouge"
+    moulin_args = " ".join(sys.argv[1:])
+    generator.rule("rouge", f"rouge {moulin_args} -fi $image_name -o $out")
+    generator.newline()
+
+
+def gen_build(generator: ninja_syntax.Writer, images: List[RougeImage]):
+    "Generate build rules for given list of images"
+    for image in images:
+        block_entry = construct_entry(image.node)
+        generator.build(f"{image.name}.img",
+                        "rouge",
+                        block_entry.get_deps(),
+                        variables=dict(image_name=image.name),
+                        pool="console")
+        generator.build(f"image-{image.name}", "phony", f"{image.name}.img")
