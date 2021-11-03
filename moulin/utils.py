7265bc772dcd8faa6d4d8f2d7375fd136d6596fb
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2021-11-03 02:18:30 +0200

    moulin: remove old helpers
    As all code uses new YAML parsing API, we can remove old
    helpers.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- moulin/utils.py
+++ moulin/utils.py
@@ -3,7 +3,6 @@
 """Module hosting different utility functions"""
 
 import os.path
-from typing import List, Union, Any
 
 
 def create_stamp_name(*args):
@@ -13,14 +12,3 @@ def create_stamp_name(*args):
         ".stamps",
         stamp.replace("-", "--").replace(os.sep, "-").replace(":", "-"))
     return os.path.abspath(path)
-
-
-def flatten_list(lst: List[Union[List, Any]]) -> List[Any]:
-    """Flatten list of lists"""
-    result = []
-    for elm in lst:
-        if isinstance(elm, list):
-            result.extend(elm)
-        else:
-            result.append(elm)
-    return result
