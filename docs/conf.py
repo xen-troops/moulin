a113967a91ac710021922da52852afb177bff7d5
Dmytro Semenets <dmytro_semenets@epam.com>
2023-04-15 12:59:39 +0300

    moulin: bump version to 0.17
    Added 'vars' oprional parameter to the zephyr builder
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021-2023, EPAM Systems'
 author = 'EPAM Systems'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.16'
+release = 'v0.17'
 
 # -- General configuration ---------------------------------------------------
 
