xen-troops/moulin

commit 19d2ec09c4368df251b2593795c85e95f9d8e87a

git.py: Fail on different branches for same repo
  doma:
    sources:
      - type: git
        url: https://github.com/xen-troops/meta-example.git
        rev: "ABCDEF"
  ...
  domu:
    sources:
      - type: git
        url: https://github.com/xen-troops/meta-example.git
        rev: "branch_A"

For doma and domu, the same GitHub repository is specified, but with
different branches.

In the case of such an erroneous YAML file, Moulin will not highlight
this error. Consequently, the user will also spend significant effort
to understand the reason for the incorrect functioning.

This patch adds an additional check to the git.py file. Now, if the
repository branches don't match, an error is displayed in the console,
and the generation of the build.ninja file is halted.

Co-Authored-By: Ihor Usyk <ihor_usyk@epam.com>
Signed-off-by: Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

moulin/fetchers/git.py
@@ -5,6 +5,7 @@
 import os.path
 from typing import List
 import pygit2
+from moulin.yaml_helpers import YAMLProcessingError
 from moulin.yaml_wrapper import YamlValue
 from moulin.utils import create_stamp_name
 from moulin import ninja_syntax
@@ -44,7 +45,7 @@ def _guess_dirname(url: str):
     return url.split("/")[-1]
 
 
-_SEEN_REPOS = []
+_SEEN_REPOS_REV = {}
 
 
 class GitFetcher:
@@ -63,11 +64,17 @@ class GitFetcher:
         clone_target = self.git_dir
         checkout_stamp = create_stamp_name(self.build_dir, self.url, "checkout")
 
-        # Do not checkout repos for the second time
-        if checkout_stamp in _SEEN_REPOS:
-            return checkout_stamp
+        if checkout_stamp in _SEEN_REPOS_REV:
+            if self.git_rev != _SEEN_REPOS_REV[checkout_stamp]:
+                # Fail on occurrence of different revision for the already downloaded repository
+                raise YAMLProcessingError(f"ERROR: Repository {self.url} has two revisions '{self.git_rev}' "
+                                          f"and '{_SEEN_REPOS_REV[checkout_stamp]}'", self.conf["rev"].mark)
+            else:
+                # Do not checkout repos for the second time
+                return checkout_stamp
 
-        _SEEN_REPOS.append(checkout_stamp)
+        _SEEN_REPOS_REV[checkout_stamp] = self.git_rev
+
         self.generator.build(clone_target,
                              "git_clone",
                              variables={

commit 7048671cc7ecb9fa5d747fb5dfdca51326996d70

git.py: added directory tracking to the Moulin git builder
Change the behavior of the git fetcher so that it doesn't consider two
or more occurrences of the same git repository in the moulin *.yaml
configuration file as an error if the duplicated repository is cloned
into different sub-folders within the build directory.
For example:

  domd:
     sources:
      - type: git
        url: "https://github.com/xen-troops/meta-example.git"
        rev: ABCDEF
        dir: "dir1"
  domu:
    sources:
      - type: git
        url: "https://github.com/xen-troops/meta-example.git"
        rev: branch_A
        dir: "dir2"
Above, identical repositories have different branches. But, also, they
have different 'dir' parameters. So that would not be considered as an
error.

But if they would have identical directories - the error still will be
triggered.

This fix adds the value, that is specified by the user as a 'dir'
parameter of the moulin's 'git' fetcher to the build.ninja file
markers, created during the execution of 'git clone' and 'git checkout'
operations. After this change, if repositories with the same name have
different directories, unique clone_stamp and checkout_stamp file
markers will be created for each repository. In this case, revision
checking of such repositories will not be performed, which is the
desired behavior.

Signed-off-by: Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
Fixes: 19d2ec09c436 ("git.py: Fail on different branches for same repo")

moulin/fetchers/git.py
@@ -69,8 +69,8 @@ class GitFetcher:
     def gen_fetch(self):
         """Generate instruction to fetch git repo"""
         clone_target = self.git_dir
-        clone_stamp = create_stamp_name(self.build_dir, self.url, "clone")
-        checkout_stamp = create_stamp_name(self.build_dir, self.url, "checkout")
+        clone_stamp = create_stamp_name(self.build_dir, clone_target, self.url, "clone")
+        checkout_stamp = create_stamp_name(self.build_dir, clone_target, self.url, "checkout")
 
         if checkout_stamp in _SEEN_REPOS_REV:
             if self.git_rev != _SEEN_REPOS_REV[checkout_stamp]: