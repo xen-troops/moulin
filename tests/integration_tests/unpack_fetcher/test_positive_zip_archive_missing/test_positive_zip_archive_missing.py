bd03641591659301f2a935ea026b584c95f18e13
Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
2024-01-18 00:13:52 +0200

    integration_tests: add the integration tests for task #95
    Added tests are focused on the following issue:
    https://github.com/xen-troops/moulin/issues/95
    
    Signed-off-by: Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    [Volodymyr: removed time.sleep() calls]
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ tests/integration_tests/unpack_fetcher/test_positive_zip_archive_missing/test_positive_zip_archive_missing.py
@@ -0,0 +1,24 @@
+import subprocess
+import pytest
+import os
+import tempfile
+
+
+@pytest.mark.integration
+def test_positive_zip_archive_missing():
+    script_path = os.path.abspath(__file__)
+    script_dir_path = os.path.dirname(script_path)
+    yaml_file = os.path.join(script_dir_path, "resources/test_positive_zip_archive_missing.yaml")
+
+    with tempfile.TemporaryDirectory(dir=script_dir_path) as tmp_dir:
+
+        result = subprocess.run(["python", "../../../../../moulin.py", yaml_file],
+                                cwd=tmp_dir,
+                                stderr=subprocess.PIPE,
+                                text=True)
+
+        assert result.returncode == 0, ("The return code is equal to '0'")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
