0a6cfc4f5f526b8cc4ef3093a5f9ae8d18c5d20c
Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
2024-01-17 23:50:11 +0200

    unpack.py: add dynamic dependecies to the unpack fetcher
    Enable building the build.ninja even when the moulin YAML configuration
    specifies an archive that is not present in the file system.
    
    Detection of the missing archive error will now occur during the ninja
    invocation, specifically when building a ninja target that unpacks the
    archive.
    
    Implement functionality to generate dyndep files for archives. This
    will ensure that Ninja will unpack archive if it was updated or
    replace any missing files. This functionality is achieved by calling
    moulin.fetchers.archive module using python. In this way we can use
    the services provided by Moulin to generate Ninja dyndep file.
    
    Signed-off-by: Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
    [Volodymyr: added dyndep generator via "python -m" call]
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- moulin/fetchers/unpack.py
+++ moulin/fetchers/unpack.py
@@ -2,12 +2,20 @@
 # Copyright 2022 EPAM Systems
 """Unpacker fetcher module"""
 
+import sys
 import os.path
 import subprocess
 from typing import List
 from moulin.yaml_helpers import YAMLProcessingError
 from moulin.yaml_wrapper import YamlValue
 from moulin import ninja_syntax
+from moulin.utils import create_stamp_name
+import logging
+
+if __name__ != "__main__":
+    log = logging.getLogger(__name__)
+else:
+    log = logging.getLogger("moulin.fetcher.unpack")
 
 
 def get_fetcher(conf: YamlValue, build_dir: str, generator: ninja_syntax.Writer):
@@ -18,12 +26,55 @@ def get_fetcher(conf: YamlValue, build_dir: str, generator: ninja_syntax.Writer)
 def gen_build_rules(generator: ninja_syntax.Writer):
     """Generate build rules using Ninja generator"""
     generator.rule("tar_unpack",
-                   command="mkdir -p $out_dir && tar -m -C $out_dir -xf $in",
+                   command="mkdir -p $out_dir && tar -m -C $out_dir -xf $in && touch $out",
                    description="Unpack $in with tar")
     generator.rule("zip_unpack",
-                   command="mkdir -p $out_dir && unzip -DD -n $in -d $out_dir",
+                   command="mkdir -p $out_dir && unzip -DD -n $in -d $out_dir && touch $out",
                    description="Unpack $in with unzip")
+    generator.rule("unpack_fetcher_dyndep",
+                   command='python3 -m "moulin.fetchers.unpack" gen_dyndep $in $out $type $outdir',
+                   description="List files for $in")
     generator.newline()
+
+
+def main() -> None:
+    """
+    Entry point for unpack module. This function is supposed to be called
+    by build system (aka Ninja) to generate dyndep file, which contains list of
+    the archive contents
+    """
+    if sys.argv[1] != "gen_dyndep":
+        log.error("Module supports only gen_dyndep operation")
+        sys.exit(1)
+    fname = sys.argv[2]
+    outfile = open(sys.argv[3], "w")
+    archive_type = sys.argv[4]
+    outdir = sys.argv[5]
+    generator = ninja_syntax.Writer(outfile)
+    generator.variable("ninja_dyndep_version", "1")
+    contents = _get_archive_file_list(archive_type, fname, outdir)
+    generator.build(_create_stamp_name(outdir, fname),
+                    "dyndep",
+                    implicit_outputs=contents,
+                    variables=dict(restat=1))
+
+
+def _get_archive_file_list(archive_type: str, fname: str, out_dir: str) -> List[str]:
+    "Get list of files in archive"
+
+    commands = {
+        "tar": ["tar", "--list", "-f", f"{fname}"],
+        "zip": ["unzip", "-Z", "-2", f"{fname}"]
+    }
+    ret = subprocess.run(commands[archive_type],
+                         check=True,
+                         stdout=subprocess.PIPE,
+                         universal_newlines=True)
+    return [os.path.join(out_dir, x) for x in ret.stdout.split("\n") if len(x) > 0]
+
+
+def _create_stamp_name(outdir: str, fname: str):
+    return create_stamp_name(outdir, fname)
 
 
 class UnpackFetcher:
@@ -46,27 +97,29 @@ class UnpackFetcher:
             raise YAMLProcessingError(f"Unkown archive type: {self.type}",
                                       self.conf["archive_type"].mark)
 
-        if not os.path.exists(self.fname):
-            raise YAMLProcessingError(f"File \"{self.fname}\" does not exist",
-                                      self.conf["file"].mark)
-
     def gen_fetch(self) -> List[str]:
         """Generate instructions to unpack archive"""
         rule_name = f"{self.type}_unpack"
-        files = self.get_file_list()
-        self.generator.build(files, rule_name, self.fname, variables={"out_dir": self.out_dir})
+        stamp = _create_stamp_name(self.out_dir, self.fname)
+        dyndep = create_stamp_name(self.out_dir, self.fname, "dyndep")
+        self.generator.build(stamp,
+                             rule_name,
+                             self.fname,
+                             order_only=[dyndep],
+                             variables={"out_dir": self.out_dir},
+                             dyndep=dyndep)
+        self.generator.build(dyndep,
+                             "unpack_fetcher_dyndep",
+                             self.fname,
+                             variables=dict(outdir=self.out_dir, type=self.type))
 
-        return files
+        return stamp
 
     def get_file_list(self) -> List[str]:
         "Get list of files in archive"
 
-        commands = {
-            "tar": ["tar", "--list", "-f", f"{self.fname}"],
-            "zip": ["unzip", "-Z", "-2", f"{self.fname}"]
-        }
-        ret = subprocess.run(commands[self.type],
-                             check=True,
-                             stdout=subprocess.PIPE,
-                             universal_newlines=True)
-        return [os.path.join(self.out_dir, x) for x in ret.stdout.split("\n") if len(x) > 0]
+        return _get_archive_file_list(self.type, self.fname, self.out_dir)
+
+
+if __name__ == "__main__":
+    main()
