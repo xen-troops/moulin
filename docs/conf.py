ccd8344d825c09df0fd51b70a0ec84062b7481d7
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2024-06-06 13:42:59 +0300

    moulin: bump version to 0.24
    There is a new "http" fetcher added. Bump up moulin version, to moulin
    configs can use "min_version" to be sure that http fetcher is present.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021-2024, EPAM Systems'
 author = 'EPAM Systems'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.23'
+release = 'v0.24'
 
 # -- General configuration ---------------------------------------------------
 
