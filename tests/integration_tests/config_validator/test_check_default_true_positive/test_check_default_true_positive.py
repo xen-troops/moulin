f25257756a412d1936e5ee10835c3b9b73020ab8
Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>
2025-06-20 20:35:01 +0300

    Add pytests for validation of missing "default: true" in parameters
    Added pytests for the following scenarios:
    -- YAML parameter does not contain a "default: true" variant
    (negative case)
    -- YAML parameter contains a "default: true" variant (positive case)
    
    Also, the unit test "test_config.py" was updated because
    "default: true" is required for all parameters in the YAML
    configuration.
    
    Signed-off-by: Mykhailo Androsiuk <mykhailo_androsiuk@epam.com>

---
+++ tests/integration_tests/config_validator/test_check_default_true_positive/test_check_default_true_positive.py
@@ -0,0 +1,24 @@
+import subprocess
+import pytest
+import os
+import tempfile
+
+
+@pytest.mark.integration
+def test_check_default_true_positive():
+    script_path = os.path.abspath(__file__)
+    script_dir_path = os.path.dirname(script_path)
+    yaml_file = os.path.join(script_dir_path, "resources/test_check_default_true_positive.yaml")
+
+    with tempfile.TemporaryDirectory(dir=script_dir_path) as tmp_dir:
+
+        result = subprocess.run(["python", "../../../../../moulin.py", yaml_file],
+                                cwd=tmp_dir,
+                                stderr=subprocess.PIPE,
+                                text=True)
+
+        assert result.returncode == 0, ("The return code is equal to '0'")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
