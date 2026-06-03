4d5a34d6673d6462cad4c52723741ddd1625fba2
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2026-06-04 01:27:11 +0300

    tests: unittests: yocto: add Yocto unit tests
    We already have integration tests for Yocto builder,
    but some things easier to test in place
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
    Tested-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

---
+++ tests/unittest/test_yocto_builder.py
@@ -0,0 +1,100 @@
+"""
+Tests for Yocto builder
+"""
+
+import unittest
+from unittest.mock import patch, ANY
+
+import yaml
+
+from moulin.build_generator import generate_build
+from moulin.build_conf import MoulinConfiguration
+
+
+class TestYoctoBuilder(unittest.TestCase):
+
+    def setUp(self):
+        patcher = patch('moulin.ninja_syntax.Writer')
+        self.Writer = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        patcher = patch('moulin.build_generator.open')
+        self.opener = patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def test_minimal(self):
+        doc = """
+desc: "Test build"
+components:
+  test:
+    builder:
+      type: "yocto"
+      build_target: core-image-minimal
+      conf:
+      target_images:
+        - "target-image"
+    sources:
+      - type: "null"
+        """
+        node = yaml.compose(doc)
+        conf = MoulinConfiguration(node)
+        generate_build(conf, "test.yaml")
+        self.Writer.return_value.build.assert_any_call(["test/build/target-image"],
+                                                       "yocto_build",
+                                                       ANY,
+                                                       variables=ANY)
+
+    def test_default_distro(self):
+        doc = """
+desc: "Test build"
+components:
+  test:
+    builder:
+      type: "yocto"
+      build_target: core-image-minimal
+      conf:
+      target_images:
+        - "target-image"
+    sources:
+      - type: "null"
+        """
+        node = yaml.compose(doc)
+        conf = MoulinConfiguration(node)
+        generate_build(conf, "test.yaml")
+        # Find build call and analyze variables
+        for call in self.Writer.return_value.build.call_args_list:
+            if call.args[1] == 'yocto_build':
+                self.assertIn("variables", call.kwargs)
+                self.assertIn("distro_dir", call.kwargs["variables"])
+                self.assertEqual(call.kwargs["variables"]["distro_dir"], "poky")
+                break
+        else:
+            self.fail("Could not find yocto_build target")
+
+    def test_set_distro(self):
+        doc = """
+desc: "Test build"
+components:
+  test:
+    builder:
+      type: "yocto"
+      build_target: core-image-minimal
+      base_distro: test-distro
+      conf:
+      target_images:
+        - "target-image"
+    sources:
+      - type: "null"
+        """
+        node = yaml.compose(doc)
+        conf = MoulinConfiguration(node)
+        generate_build(conf, "test.yaml")
+        # Find build call and analyze variables
+        for call in self.Writer.return_value.build.call_args_list:
+            if call.args[1] == 'yocto_build':
+                self.assertIn("variables", call.kwargs)
+                self.assertIn("distro_dir", call.kwargs["variables"])
+                self.assertEqual(call.kwargs["variables"]["distro_dir"], "test-distro")
+                break
+        else:
+            self.fail("Could not find yocto_build target")
